17b38ffcff80067eeec15e5e11e0f49771788137
Crystal <muthonigachanja2000@gmail.com>
2023-06-11 22:52:31 -0700

    A few changes

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
+const h4 = document.createElement("h4");
+h4.textContent = "Contains Javascript";
 
-document.querySelector("body").appendChild(h2);
+document.querySelector("body").appendChild(h4);
